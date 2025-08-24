dfa3a51c0c6be0a6ff0ac6dbdb584a59ddb1dabc
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-24 22:40:44 +0530

    Solved task third where i made a conditional logic of comparing two number and finding which one is greater

---
+++ task3.js
@@ -0,0 +1,11 @@
+// Comparing two numbers and finding which one is greater
+
+// Declaring the variable for input
+const number1 = 28;
+const number2 = 24;
+// Logic
+if(number1 > number2) {
+    console.log("First Number:",number1,"is greater");
+} else {
+    console.log("Second Number:",number2,"is greater");
+}
