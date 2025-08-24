903d109eda05365f666f7510ee2deae118952f05
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-24 22:05:47 +0530

    Solved the task for seprating odd and even number

---
+++ task1.js
@@ -0,0 +1,14 @@
+// Even or Odd checker
+
+
+// Creating a variable for storing input
+let number = 16;
+
+// Condtionals with checking formula of number % 2 === 0 (This formula is for checking even numbers and seperating them out, As it checks if the reminder comes 0 after deviding to the number '2' or not.) 
+
+if(number % 2 === 0) {
+    console.log("The number",number,"is even");
+} else {
+    console.log("The number",number,"is odd");
+}
+
