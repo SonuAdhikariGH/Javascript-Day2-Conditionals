7e0dc5ddddc82b08f39df2c22ac8afc858d7aa98
Rohitdevatpy <227948873+SonuAdhikariGH@users.noreply.github.com>
2025-08-24 21:57:03 +0530

    Starting the day 2 with conditionals and then will head up to tasks and functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Conditionals if/else
+let age = 13;
+
+
+// Condtionals will only log if the condition us true or else it will push the else functions after checking the if
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are not an adult");
+}
